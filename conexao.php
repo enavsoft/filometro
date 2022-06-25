eb63389506c47a1d1cea20e7a0596dd55a182b33
enavsoft <micro_vanessa@outlook.com>
2022-06-25 01:38:30 -0300

    main

--- conexao.php
+++ conexao.php
@@ -3,7 +3,7 @@
 	$dbHost = 'localhost';
 	$dbUsername = 'enavsoft_fila';
 	$dbName = 'enavsoft_filometro';
-    $dbPassword ='filometro@123';
+    $dbPassword ='Filometro@123';
 
     $conexao = new mysqli($dbHost, $dbUsername, $dbName, $dbPassword);
 	if($conexao -> connect_errno)
